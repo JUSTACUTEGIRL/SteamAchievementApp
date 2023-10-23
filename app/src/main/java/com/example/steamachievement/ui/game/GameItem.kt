5b8fd89047e247e0149b745200edc8db4ff7d1ef
JUSTACUTEGIRL <minhmxc@gmail.com>
2023-10-24 03:09:40 +0800

    Change Image Gradient

--- app/src/main/java/com/example/steamachievement/ui/game/GameItem.kt
+++ app/src/main/java/com/example/steamachievement/ui/game/GameItem.kt
@@ -1,5 +1,6 @@
 package com.example.steamachievement.ui.game
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -16,16 +17,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.steamachievement.Game
@@ -46,13 +52,24 @@ fun GameItemCompact(
         GamePercentageComplete(game = game)
 
         Box {
-            GameImage(game.appID)
+//            Image(
+//                painter = painterResource(id = R.drawable.image),
+//                contentDescription = null,
+//                contentScale = ContentScale.FillHeight,
+//                modifier = Modifier
+//                    .aspectRatio(6f / 9f)
+//            )
+
+            GameImage(appID = game.appID)
+            Box(modifier = Modifier
+                .matchParentSize()
+                .background(Brush.verticalGradient(listOf(Color.Transparent, Color.Black.copy(alpha = 0.75f)), 1000.0f))
+            )
 
             Column(
                 modifier = Modifier
                     .align(Alignment.BottomStart)
                     .fillMaxWidth()
-                    .background(Brush.verticalGradient(listOf(Color.Transparent, Color.Black)))
             ) {
                 Text(
                     text = game.gameName,
@@ -61,11 +78,20 @@ fun GameItemCompact(
                     overflow = TextOverflow.Ellipsis,
                     color = Color.White,
                     modifier = Modifier.padding(7.dp),
-                    style = MaterialTheme.typography.bodyMedium,
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        shadow = Shadow(color = Color.Black, offset = Offset(0f, 0f), blurRadius = 15f)
+                    ),
                 )
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun GameItemCompactPreview() {
+    GameItemCompact(game = Game(gameName = "Jitsu", appID = "0"), onClickGame = { /*TODO*/ }, modifier = Modifier)
 }
 
 @Composable
